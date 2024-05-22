220232d589b24dc5ef90d107112d2c83c9c41b3d
Harsh Kachhadiya <156128726+Hkachhadiya@users.noreply.github.com>
2024-05-22 17:47:23 -0400

    Update rmse.py

--- rmse.py
+++ rmse.py
@@ -3,5 +3,5 @@ import numpy as np
 def rmse(predictions, targets):
     pred = np.array(predictions)
     tar = np.array(targets)
-    rmse = np.sqrt(np.mean((pred - tar) ** 2))
+    rmse = None
     return rmse
